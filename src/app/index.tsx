d2ffb68139770f14267c16efe8b7362b4f883727
carlos santos <rickhenriquetab@gmail.com>
2025-02-24 19:42:17 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Index() {
+  return (
+    <View>
+      <Text style={{textAlign: 'center'}}>Hello World</Text>
+    </View>
+  )
+}
