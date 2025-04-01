f1c99eee925e6b50e8471ab86b8dc5f45c93d081
carlos santos <rickhenriquetab@gmail.com>
2025-04-01 20:24:05 -0300

    rebase 1

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,41 +7,47 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 
 export default function Layout() {
     return (
-        <ThemeProvider>
-            <GestureHandlerRootView className="flex-1">
-                <SQLiteProvider databaseName="unitrain.db" onInit={InitilizeDatabase}>
-                    <Drawer>
-                        <Drawer.Screen
-                            name="(tabs)"
-                            options={{ title: 'Principal', headerShown: true }}
-                        />
-                        <Drawer.Screen
-                            name="config"
-                            options={{ title: 'Configurações', headerShown: true }}
-                        />
-                        <Drawer.Screen
-                            name="statistics"
-                            options={{ title: 'Estatísticas', headerShown: true }}
-                        />
-                        <Drawer.Screen
-                            name="(config)/theme"
-                            options={{
-                                title: "Tema",
-                                drawerItemStyle: { display: 'none' },
-                                headerShown: true,
-                            }}
-                        />
-                        <Drawer.Screen
-                            name="(config)/backup"
-                            options={{
-                                title: "Backup",
-                                drawerItemStyle: { display: 'none' },
-                                headerShown: true,
-                            }}
-                        />
-                    </Drawer>
-                </SQLiteProvider>
-            </GestureHandlerRootView>
-        </ThemeProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <SQLiteProvider databaseName="unitrain.db" onInit={InitilizeDatabase}>
+                <Drawer>
+                    <Drawer.Screen
+                        name="(tabs)"
+                        options={{ title: 'Principal', headerShown: true }}
+                    />
+                    <Drawer.Screen
+                        name="config"
+                        options={{ title: 'Configurações', headerShown: true }}
+                    />
+                    <Drawer.Screen
+                        name="statistics"
+                        options={{ title: 'Estatísticas', headerShown: true }}
+                    />
+                    <Drawer.Screen
+                        name="(config)/theme"
+                        options={{
+                            title: "Tema",
+                            drawerItemStyle: { display: 'none' },
+                            headerShown: true,
+                        }}
+                    />
+                    <Drawer.Screen
+                        name="(config)/backup"
+                        options={{
+                            title: "Backup",
+                            drawerItemStyle: { display: 'none' },
+                            headerShown: true,
+                        }}
+                    />
+                     <Drawer.Screen
+                        name="(workout)/[id]"
+                        options={{
+                            title: "Detalhe do treino",
+                            drawerItemStyle: { display: 'none' },
+                            headerShown: true,
+                        }}
+                    />
+                </Drawer>
+            </SQLiteProvider>
+        </GestureHandlerRootView>
     );
 }
