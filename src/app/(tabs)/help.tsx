2b6fc7efeb87cc8b2d96996c290bcec1e329a221
Felipe Negrelle <felipenegrelleramos@gmail.com>
2025-04-02 00:12:33 -0300

    [HELP] adicionando botão de contato com o desenvolvedor que redireciona para email

--- src/app/(tabs)/help.tsx
+++ src/app/(tabs)/help.tsx
@@ -1,9 +1,11 @@
-import {ScrollView} from "react-native";
+import {Linking, ScrollView, View} from "react-native";
 import HelpItem from "@/components/HelpItem";
+import {Ionicons} from "@expo/vector-icons";
+import Button from "@/components/Button";
 
 export default function Help() {
     return (
-        <   ScrollView contentContainerStyle={{padding: 16}}>
+        <ScrollView contentContainerStyle={{padding: 16, flexGrow: 1}}>
             <HelpItem
                 question="Como criar um treino?"
                 answer="Vá até a aba 'Treinos', clique em '+ Novo treino', escolha o dia da semana e adicione os exercícios desejados."
@@ -16,6 +18,15 @@ export default function Help() {
                 question="Como fazer backup dos meus dados?"
                 answer="Acesse as configurações e clique em 'Backup'. Você poderá exportar um arquivo .bak com seus dados."
             />
+            <View className="absolute bottom-4 right-4">
+                <Button
+                    icon={<Ionicons name="help" size={20} color="white"/>}
+                    onPress={() => {
+                        Linking.openURL('mailto:felipenegrelleramos@gmail.com?subject=Ajuda&body=Descreva%20sua%20dúvida%20aqui');
+                    }}
+                    small={true}
+                />
+            </View>
         </ScrollView>
     );
 }
