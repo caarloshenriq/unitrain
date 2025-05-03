bd23547fb8682ad6c8c0e0b93f19181c2e9b916b
carlos santos <rickhenriquetab@gmail.com>
2025-05-03 13:46:46 -0300

    corrigindo problema de atualizacao de dados do grafico

--- src/app/statistics.tsx
+++ src/app/statistics.tsx
@@ -24,7 +24,7 @@ export default function Statistics() {
       setProgressData(processed);
     }
     fetchData();
-  }, []);
+  }, [progressData]);
 
   return (
     <SafeAreaView style={styles.container}>
